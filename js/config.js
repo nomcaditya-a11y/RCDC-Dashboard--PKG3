8d28463825d3ef948901569640cff259224758c6
nomcaditya-a11y <nomcaditya@gmail.com>
2026-01-05 12:04:34 +0530

    Replace CONFIG with GET function for CSV fetching
    Replaced CONFIG export with GET function to fetch CSV data from Google Sheets.

--- js/config.js
+++ js/config.js
@@ -1,6 +1,9 @@
-export const CONFIG = {
-  SHEET_URL:
-    'https://docs.google.com/spreadsheets/d/18P13Qjm8Dz3GxNKHwoUlxBmqNkkfO7D1XmQP8oTedL4/gviz/tq?tqx=out:csv&gid=276403000',
-  REFRESH_INTERVAL: 300000,
-  AVG_REVENUE_PER_CONSUMER: 500
-};
+export async function GET() {
+  const url =
+    'https://docs.google.com/spreadsheets/d/18P13Qjm8Dz3GxNKHwoUlxBmqNkkfO7D1XmQP8oTedL4/export?format=csv&gid=276403000';
+
+  const res = await fetch(url, { cache: 'no-store' });
+  return new Response(await res.text(), {
+    headers: { 'Content-Type': 'text/csv' }
+  });
+}

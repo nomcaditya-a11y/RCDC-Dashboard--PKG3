873f122b90b402d2b3269e130d23d6ee209ef0d7
nomcaditya-a11y <nomcaditya@gmail.com>
2026-01-05 12:02:08 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
-  // CSV export using gid (more reliable than sheet name)
-  SHEET_URL: 'https://docs.google.com/spreadsheets/d/18P13Qjm8Dz3GxNKHwoUlxBmqNkkfO7D1XmQP8oTedL4/export?format=csv&gid=276403000',
-  REFRESH_INTERVAL: 300000, // 5 minutes
+  SHEET_URL:
+    'https://docs.google.com/spreadsheets/d/18P13Qjm8Dz3GxNKHwoUlxBmqNkkfO7D1XmQP8oTedL4/gviz/tq?tqx=out:csv&gid=276403000',
+  REFRESH_INTERVAL: 300000,
   AVG_REVENUE_PER_CONSUMER: 500
 };
